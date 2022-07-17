7ad83acb0655f0cba3e8ea52b7698142bb131994
Niklay Kovalev <kovalev.nikolay.89@gmail.com>
2022-07-17 15:45:37 +0300

    feat: add check brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig, index = 0) {
+  let array = str.split('');
+
+  if(array.length == 0 )
+    return true;
+
+  const configIndex = bracketsConfig.flat().indexOf(array[index]);
+
+  if(configIndex % 2 ==0){
+    if(array[index + 1] == bracketsConfig.flat()[configIndex + 1]){
+        array.splice(index, 2);
+        return check(array.join(''), bracketsConfig, 0);    
+    } else {
+        return check(array.join(''), bracketsConfig, index + 1); 
+    }
+  }
+
+  return false;
 }
